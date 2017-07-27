e545e67fd1bb640d0d6048ff2f9ddecae7a6015c
TheBitStick <thebitstick@tfwno.gf>
2017-07-26 20:02:24 -0500

    Cleaner code and Window shadows

--- src/file.rs
+++ src/file.rs
@@ -7,10 +7,19 @@ use send;
 
 pub fn image(cmd: String)
 {
+    let mut convert_area: String =
+    "convert - ( +clone -background black -shadow 80x3+5+5 ) +swap -background none -layers merge +repage ".to_owned();
+    let mut convert_window: String =
+    "convert ".to_owned();
     let mut tmp = env::temp_dir();
     tmp.push("sharexin.png");
     let temp = tmp.to_str().unwrap().clone();
-    if cmd == "-s" { 
+    convert_area.push_str(temp.clone());
+    convert_window.push_str(temp.clone());
+    convert_window.push_str(" ");
+    convert_window.push_str("( +clone -background black -shadow 80x3+5+5 ) +swap -background none -layers merge +repage ");
+    convert_window.push_str(temp.clone());
+    if cmd == "-s" {
         let _before_image = Command::new("maim")
         .arg(temp.clone()).output().expect("Nope");
         println!("{}", String::from_utf8_lossy(&_before_image.stdout));
@@ -19,7 +28,7 @@ pub fn image(cmd: String)
         let _sleep = Command::new("sleep").arg("0.5").output().expect("Nope");
         println!("{}", String::from_utf8_lossy(&_sleep.stdout));
         let _image = Pipe::new("maim -s")
-        .then("convert - ( +clone -background black -shadow 80x3+5+5 ) +swap -background none -layers merge +repage /tmp/sharexin.png")
+        .then(&convert_area)
         .finally()
         .expect("Nope")
         .wait_with_output()
@@ -28,10 +37,16 @@ pub fn image(cmd: String)
         println!("{}", String::from_utf8_lossy(&_kill.stdout));
     }
     else if cmd == "-i" {
-        let _image = Command::new("maim")
-        .args(&[&cmd, "$(xdotool getactivewindow)", temp])
+        let _xdo = Command::new("xdotool").arg("getactivewindow")
         .output().expect("Nope");
-        println!("{}", String::from_utf8_lossy(&_image.stdout));
+        let mut _before_command = String::from("maim -i ");
+        _before_command.push_str(&String::from_utf8_lossy(&_xdo.stdout));
+        _before_command.push_str(" ");
+        _before_command.push_str(&temp.clone());
+        let _before_image = Pipe::new(&_before_command)
+        .finally().expect("Nope").wait_with_output().expect("Nope");
+        let _image = Pipe::new(&convert_window)
+        .finally().expect("Nope").wait_with_output().expect("NopeNope");
     }
     else {
         let _image = Command::new("maim")
--- src/main.rs
+++ src/main.rs
@@ -1,127 +1,16 @@
+extern crate pipers;
+extern crate libnotify;
 extern crate gtk;
 extern crate glib;
 extern crate gdk;
-extern crate time;
-extern crate pipers;
-extern crate libnotify;
-use std::rc::Rc;
-use std::cell::RefCell;
-use gdk::enums::key;
-use gtk::traits::*;
-use gtk::*;
 use std::*;
 mod send;
 mod file;
 mod help;
+mod gui;
+use gui::gui;
 
-static VERSION: &'static str = "sharexin 0.3.3";
-
-fn gui(mort: bool, morti: bool)
-{
-    if gtk::init().is_err() {
-        println!("GTK did not init.");
-        return;
-    }
-
-    let builder = gtk::Builder::new_from_string(include_str!("sharexin.glade"));
-    let window: gtk::Window = builder.get_object("window").unwrap();
-    let header: gtk::HeaderBar = builder.get_object("header").unwrap();
-    let text: gtk::TextView = builder.get_object("text").unwrap();
-    let cancel: gtk::Button = builder.get_object("cancel").unwrap();
-    let send: gtk::Button = builder.get_object("send").unwrap();
-    window.set_title("ShareXin");
-    if mort {
-        header.set_subtitle("Mastodon");
-    }
-    else {
-        header.set_subtitle("Twitter");
-    }
-
-    window.connect_delete_event(|_, _| {
-        gtk::main_quit();
-        Inhibit(false)
-    });
-
-    cancel.connect_clicked(|_| {
-        gtk::main_quit();
-    });
-
-    let wrap_send = Rc::new(RefCell::new(send));
-
-    {
-        let send = wrap_send.clone();
-        send.borrow().connect_clicked(move |_| {
-            let buffer = TextView::get_buffer(&text).unwrap();
-            let sent: Option<String> = TextBuffer::get_text(&buffer,
-            &TextBuffer::get_start_iter(&buffer), 
-            &TextBuffer::get_end_iter(&buffer), false);
-            let tweet: String = sent.unwrap();
-            if mort {
-                if morti {
-                    thread::spawn(move || {
-                        glib::idle_add(move || {
-                            send::toot_img(tweet.clone());
-                            gtk::main_quit();
-                            Continue(false)
-                        });
-                     });
-                }
-                else if !tweet.is_empty() {
-                    thread::spawn(move || {
-                        glib::idle_add(move || {
-                            send::toot(tweet.clone());
-                            gtk::main_quit();
-                            Continue(false)
-                        });
-                     });
-                }
-                else {
-                    send::notification_3(mort);
-                    gtk::main_quit();
-                }
-            }
-            else {
-                if morti {
-                    thread::spawn(move || {
-                        glib::idle_add(move || {
-                            send::twitter_img(tweet.clone());
-                            gtk::main_quit();
-                            Continue(false)
-                        });
-                     });
-                }
-                else if !tweet.is_empty() {
-                    thread::spawn(move || {
-                        glib::idle_add(move || {
-                            send::twitter(tweet.clone());
-                            gtk::main_quit();
-                            Continue(false)
-                        });
-                     });
-                }
-                else {
-                    send::notification_3(mort);
-                    gtk::main_quit();
-                }
-            }
-        });
-        }
-    {
-        let send = wrap_send.clone();
-        window.connect_key_press_event(move |_,key| {
-            if key.get_state().intersects(gdk::CONTROL_MASK) {
-                match key.get_keyval() {
-                    key::Return => send.borrow().clicked(),
-                    _ => ()
-                }
-            }
-            Inhibit(false)
-        });
-    }
-
-    window.show_all();
-    gtk::main();
-}
+static VERSION: &'static str = "sharexin 0.3.4";
 
 fn main()
 {
@@ -146,12 +35,8 @@ fn main()
                 file::image(String::from("-i"));
                 gui(false, true);
             },
-            "-nt" => {
-                gui(false, false);
-            },
-            "-nm" => {
-                gui(true, false);
-            },
+            "-nt" => gui(false, false),
+            "-nm" => gui(true, false),
             "-m" => {
                 file::image(String::new());
                 gui(true, true);
@@ -160,15 +45,9 @@ fn main()
                 file::image(String::new());
                 gui(false, true);
             },
-            "-af" => {
-                file::image(String::from("-s"));
-            },
-            "-wf" => {
-                file::image(String::from("-i"));
-            },
-            "-f" => {
-                file::image(String::new());
-            },
+            "-af" => file::image(String::from("-s")),
+            "-wf" => file::image(String::from("-i")),
+            "-f" => file::image(String::new()),
             _ => help::help()
         }
     }
