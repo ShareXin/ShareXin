1399b4df9e2a5c24dd54e7e39304ad3cb518ed7a
Bitstick <thebitstick@tfwno.gf>
2017-09-01 07:23:41 -0500

    Added from example
    https://github.com/qdot/systray-rs/blob/master/examples/systray-example.rs

---
+++ src/systray.rs
@@ -0,0 +1,24 @@
+extern crate systray;
+
+fn systray() {
+    let mut app;
+    match systray::Application::new() {
+        Ok(w) => app = w,
+        Err(_) => panic!("Can't create systray!")
+    }
+    app.set_icon_from_file(&"/usr/share/gxkb/flags/ua.png".to_string()).ok();
+    app.add_menu_item(&"Window".to_string(), |_| {
+        // Nothing
+    }).ok();
+    app.add_menu_item(&"Tweet".to_string(), |window| {
+        window.add_menu_item(&"Interior item".to_string(), |_| {
+            // Tweet window screenshot
+        }).ok();
+        window.add_menu_separator().ok();
+    }).ok();
+    app.add_menu_separator().ok();
+    app.add_menu_item(&"Quit".to_string(), |window| {
+        window.quit();
+    }).ok();
+    app.wait_for_message();
+}
