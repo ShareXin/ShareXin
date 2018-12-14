a1f03c4e17c3151496b8fc1dde0d2ab1e4c5e7fa
thebitstick <thebitstick@tfwno.gf>
2018-12-13 21:15:44 -0600

    Fixed bug in UI
    Would remain as a "zombie" process after sending a tweet/toot. Fixed, and restructured.

--- src/dialog.rs
+++ src/dialog.rs
@@ -33,69 +33,131 @@ macro_rules! clone {
         }
     );
 }
+pub fn dialog(service: ServiceKind, message: MessageKind) {
+    let application =
+        gtk::Application::new("io.github.ShareXin", gio::ApplicationFlags::NON_UNIQUE)
+            .expect(&text::message(24));
+    glib::set_prgname(Some("ShareXin"));
+    application.connect_startup(move |app| {
+        // Creates variables for objects in Glade GTK file
+        let builder = gtk::Builder::new_from_string(include_str!("../data/gtk/dialog.ui"));
+        let window: gtk::Window = builder.get_object("window").unwrap();
+        let header: gtk::HeaderBar = builder.get_object("header").unwrap();
+        let text: gtk::TextView = builder.get_object("text").unwrap();
+        let buffer: gtk::TextBuffer = builder.get_object("buffer").unwrap();
+        let cancel: gtk::Button = builder.get_object("cancel").unwrap();
+        let send: gtk::Button = builder.get_object("send").unwrap();
+        let image: gtk::Button = builder.get_object("image").unwrap();
+        let count: gtk::Label = builder.get_object("count").unwrap();
+        window.set_application(app);
 
-fn build_ui(application: &gtk::Application, service: ServiceKind, message: MessageKind) {
-    // Creates variables for objects in Glade GTK file
-    let builder = gtk::Builder::new_from_string(include_str!("../data/gtk/dialog.ui"));
-    let window: gtk::Window = builder.get_object("window").unwrap();
-    let header: gtk::HeaderBar = builder.get_object("header").unwrap();
-    let text: gtk::TextView = builder.get_object("text").unwrap();
-    let buffer: gtk::TextBuffer = builder.get_object("buffer").unwrap();
-    let cancel: gtk::Button = builder.get_object("cancel").unwrap();
-    let send: gtk::Button = builder.get_object("send").unwrap();
-    let image: gtk::Button = builder.get_object("image").unwrap();
-    let count: gtk::Label = builder.get_object("count").unwrap();
-    window.set_application(application);
+        // Set Headerbar Subtitle and Default Character Count Label
+        match service {
+            ServiceKind::Twitter => {
+                header.set_subtitle("Twitter");
+                match message {
+                    MessageKind::Image => count.set_label(&TWITTER_IMAGE_COUNT.to_string()),
+                    MessageKind::Text => count.set_label(&TWITTER_COUNT.to_string()),
+                };
+            }
+            ServiceKind::Mastodon => {
+                header.set_subtitle("Mastodon");
+                count.set_label(&MASTODON_COUNT.to_string());
+            }
+            ServiceKind::Imgur => unreachable!("Imgur does not open a GTK dialog"),
+        }
 
-    // Set Headerbar Subtitle and Default Character Count Label
-    match service {
-        ServiceKind::Twitter => {
-            header.set_subtitle("Twitter");
-            match message {
-                MessageKind::Image => count.set_label(&TWITTER_IMAGE_COUNT.to_string()),
-                MessageKind::Text => count.set_label(&TWITTER_COUNT.to_string()),
+        // If user is not sending an image, then remove view screenshot button, and disallow user to send
+        if let MessageKind::Text = message {
+            image.destroy();
+            send.set_sensitive(false);
+        }
+
+        // Opens screenshot made by user
+        image.connect_clicked(move |_| {
+            image::open_temp();
+            return;
+        });
+
+        // When window deleted by user (closed), quit GTK and delete temporary image if possible
+        window.connect_delete_event(move |window, _| {
+            window.destroy();
+            if let MessageKind::Image = message {
+                image::delete_temp();
+            }
+            Inhibit(false)
+        });
+
+        // When cancel button clicked, quit GTK and delete temporary image if possible
+        cancel.connect_clicked(clone!(window => move |_| {
+            window.destroy();
+            if let MessageKind::Image = message {
+                image::delete_temp();
+            }
+            Inhibit(false);
+        }));
+
+        // When send button clicked
+        send.connect_clicked(clone!(buffer,window => move |_| {
+            // Get String from Text Buffer (text entered by user)
+            let status: String = match TextBuffer::get_text(
+                &buffer,
+                &TextBuffer::get_start_iter(&buffer),
+                &TextBuffer::get_end_iter(&buffer),
+                false,
+            ) {
+                Some(string) => string,
+                None => String::new()
             };
-        }
-        ServiceKind::Mastodon => {
-            header.set_subtitle("Mastodon");
-            count.set_label(&MASTODON_COUNT.to_string());
-        }
-        ServiceKind::Imgur => unreachable!("Imgur does not open a GTK dialog"),
-    }
 
-    // If user is not sending an image, then remove view screenshot button, and disallow user to send
-    if let MessageKind::Text = message {
-        image.destroy();
-        send.set_sensitive(false);
-    }
+            // Checks if Twitter or Mastodon,
+            // then checks if status is being sent with an image or not,
+            // then decides what to do with the status
+            // Creates thread to be able to close the GTK window while sending status/image
+            match service {
+                ServiceKind::Twitter => match message {
+                    MessageKind::Image => {
+                            glib::idle_add(move || {
+                                twitter::image(status.clone());
+                                Continue(false)
+                            });
+                        window.destroy();
+                    }
+                    MessageKind::Text => {
+                        if !status.is_empty() {
+                                glib::idle_add(move || {
+                                    twitter::tweet(status.clone());
+                                    Continue(false)
+                                });
+                            window.destroy();
+                        }
+                    }
+                },
+                ServiceKind::Mastodon => match message {
+                    MessageKind::Image => {
+                            glib::idle_add(move || {
+                                mastodon::image(status.clone());
+                                Continue(false)
+                            });
+                        window.destroy();
+                    }
+                    MessageKind::Text => {
+                        if !status.is_empty() {
+                                glib::idle_add(move || {
+                                    mastodon::toot(status.clone());
 
-    // Opens screenshot made by user
-    image.connect_clicked(move |_| {
-        image::open_temp();
-        return;
-    });
+                                    Continue(false)
+                                });
+                            window.destroy();
+                        }
+                    }
+                },
+                ServiceKind::Imgur => unreachable!("Imgur does not open a GTK dialog"),
+            }
+        }));
 
-    // When window deleted by user (closed), quit GTK and delete temporary image if possible
-    window.connect_delete_event(move |window, _| {
-        window.destroy();
-        if let MessageKind::Image = message {
-            image::delete_temp();
-        }
-        Inhibit(false)
-    });
-
-    // When cancel button clicked, quit GTK and delete temporary image if possible
-    cancel.connect_clicked(clone!(window => move |_| {
-        window.destroy();
-        if let MessageKind::Image = message {
-            image::delete_temp();
-        }
-        Inhibit(false);
-    }));
-
-    // When send button clicked
-    send.connect_clicked(clone!(buffer,window => move |_| {
-        // Get String from Text Buffer (text entered by user)
+        // Character count when keys are pressed in the Text Box
+        text.connect_key_release_event(clone!(send,count => move |_, _| {
         let status: String = match TextBuffer::get_text(
             &buffer,
             &TextBuffer::get_start_iter(&buffer),
@@ -105,155 +167,82 @@ fn build_ui(application: &gtk::Application, service: ServiceKind, message: Messa
             Some(string) => string,
             None => String::new()
         };
+        let status_count = char_count(service, status, message);
 
-        // Checks if Twitter or Mastodon,
-        // then checks if status is being sent with an image or not,
-        // then decides what to do with the status
-        // Creates thread to be able to close the GTK window while sending status/image
-        match service {
-            ServiceKind::Twitter => match message {
-                MessageKind::Image => {
-                        glib::idle_add(move || {
-                            twitter::image(status.clone());
-                            gtk::main_quit();
-                            Continue(false)
-                        });
-                    window.hide();
-                }
-                MessageKind::Text => {
-                    if !status.is_empty() {
-                            glib::idle_add(move || {
-                                twitter::tweet(status.clone());
-                                gtk::main_quit();
+        // uses markdown to set color
+        let mut limit = format!("<span foreground=\"#EE0456\">");
+        limit.push_str(&status_count.to_string());
+        limit.push_str("</span>");
+        let mut hit = format!("<span foreground=\"#ECA60B\">");
+        hit.push_str(&status_count.to_string());
+        hit.push_str("</span>");
 
-                                Continue(false)
-                            });
-                        window.hide();
+        match message {
+            MessageKind::Image => match service {
+                ServiceKind::Twitter => {
+                    if status_count <= 20 && status_count >= 0 {
+                        count.set_markup(&hit);
+                    } else if status_count < 0 {
+                        count.set_markup(&limit);
+                    } else {
+                        count.set_label(&status_count.to_string());
                     }
                 }
-            },
-            ServiceKind::Mastodon => match message {
-                MessageKind::Image => {
-                        glib::idle_add(move || {
-                            mastodon::image(status.clone());
-                            gtk::main_quit();
-                            Continue(false)
-                        });
-                    window.hide();
-                }
-                MessageKind::Text => {
-                    if !status.is_empty() {
-                            glib::idle_add(move || {
-                                mastodon::toot(status.clone());
-                                gtk::main_quit();
-
-                                Continue(false)
-                            });
-                        window.hide();
+                ServiceKind::Mastodon => {
+                    if status_count < 0 {
+                        count.set_markup(&limit);
+                    } else {
+                        count.set_label(&status_count.to_string());
                     }
                 }
+                ServiceKind::Imgur => unreachable!("Imgur does not open a GTK dialog"),
             },
-            ServiceKind::Imgur => unreachable!("Imgur does not open a GTK dialog"),
+            MessageKind::Text => match service {
+                ServiceKind::Twitter => {
+                    if status_count >= TWITTER_COUNT || status_count < 0 {
+                        send.set_sensitive(false);
+                    } else {
+                        send.set_sensitive(true);
+                    }
+                    if status_count <= 20 && status_count >= 0 {
+                        count.set_markup(&hit);
+                    } else if status_count < 0 {
+                        count.set_markup(&limit);
+                    } else {
+                        count.set_label(&status_count.to_string());
+                    }
+                }
+                ServiceKind::Mastodon => {
+                    if status_count >= MASTODON_COUNT || status_count < 0 {
+                        send.set_sensitive(false);
+                    } else {
+                        send.set_sensitive(true);
+                    }
+                    if status_count < 0 {
+                        count.set_markup(&limit);
+                    } else {
+                        count.set_label(&status_count.to_string());
+                    }
+                }
+                ServiceKind::Imgur => unreachable!("Imgur does not open a GTK dialog"),
+            },
         }
-    }));
+            Inhibit(false)
+        }));
 
-    // Character count when keys are pressed in the Text Box
-    text.connect_key_release_event(clone!(send,count => move |_, _| {
-    let status: String = match TextBuffer::get_text(
-        &buffer,
-        &TextBuffer::get_start_iter(&buffer),
-        &TextBuffer::get_end_iter(&buffer),
-        false,
-    ) {
-        Some(string) => string,
-        None => String::new()
-    };
-    let status_count = char_count(service, status, message);
-
-    // uses markdown to set color
-    let mut limit = format!("<span foreground=\"#EE0456\">");
-    limit.push_str(&status_count.to_string());
-    limit.push_str("</span>");
-    let mut hit = format!("<span foreground=\"#ECA60B\">");
-    hit.push_str(&status_count.to_string());
-    hit.push_str("</span>");
-
-    match message {
-        MessageKind::Image => match service {
-            ServiceKind::Twitter => {
-                if status_count <= 20 && status_count >= 0 {
-                    count.set_markup(&hit);
-                } else if status_count < 0 {
-                    count.set_markup(&limit);
-                } else {
-                    count.set_label(&status_count.to_string());
-                }
+        // Enables CTRL+Enter shortcut to send a status
+        text.connect_key_press_event(clone!(send => move |_, key| {
+            if key.get_state().intersects(gdk::ModifierType::CONTROL_MASK) &&
+                                            key.get_keyval() == gdk::enums::key::Return &&
+                                            send.get_sensitive() {
+                            send.clicked();
             }
-            ServiceKind::Mastodon => {
-                if status_count < 0 {
-                    count.set_markup(&limit);
-                } else {
-                    count.set_label(&status_count.to_string());
-                }
-            }
-            ServiceKind::Imgur => unreachable!("Imgur does not open a GTK dialog"),
-        },
-        MessageKind::Text => match service {
-            ServiceKind::Twitter => {
-                if status_count >= TWITTER_COUNT || status_count < 0 {
-                    send.set_sensitive(false);
-                } else {
-                    send.set_sensitive(true);
-                }
-                if status_count <= 20 && status_count >= 0 {
-                    count.set_markup(&hit);
-                } else if status_count < 0 {
-                    count.set_markup(&limit);
-                } else {
-                    count.set_label(&status_count.to_string());
-                }
-            }
-            ServiceKind::Mastodon => {
-                if status_count >= MASTODON_COUNT || status_count < 0 {
-                    send.set_sensitive(false);
-                } else {
-                    send.set_sensitive(true);
-                }
-                if status_count < 0 {
-                    count.set_markup(&limit);
-                } else {
-                    count.set_label(&status_count.to_string());
-                }
-            }
-            ServiceKind::Imgur => unreachable!("Imgur does not open a GTK dialog"),
-        },
-    }
-        Inhibit(false)
-    }));
+            Inhibit(false)
+        }));
 
-    // Enables CTRL+Enter shortcut to send a status
-    text.connect_key_press_event(clone!(send => move |_, key| {
-        if key.get_state().intersects(gdk::ModifierType::CONTROL_MASK) &&
-                                        key.get_keyval() == gdk::enums::key::Return &&
-                                        send.get_sensitive() {
-                        send.clicked();
-        }
-        Inhibit(false)
-    }));
-
-    // Shows the window created
-    window.show_all();
-}
-
-pub fn dialog(service: ServiceKind, message: MessageKind) {
-    let application =
-        gtk::Application::new("io.github.ShareXin", gio::ApplicationFlags::NON_UNIQUE)
-            .expect(&text::message(24));
-    glib::set_application_name("ShareXin");
-    glib::set_prgname(Some("ShareXin"));
-    application.connect_startup(clone!(application => move |_| {
-        build_ui(&application, service, message);
-    }));
+        // Shows the window created
+        window.show_all();
+    });
     application.connect_activate(|_| {});
     let args: Vec<String> = env::args().collect();
     application.run(&args);
